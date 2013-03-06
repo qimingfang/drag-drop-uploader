6c94e377b363e336c7fb9127ab3b5adbf20fd031
--replace-all <qf26@cornell.edu>
2013-03-06 00:30:32 -0500

    drag and drop uploader

---
+++ upload.php
@@ -0,0 +1,9 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+  if(move_uploaded_file($_FILES['file']['tmp_name'], "uploads/".$_FILES['file']['name'])){
+    echo "success";
+  } else {
+    echo "failure";
+  }
+}
+?>
